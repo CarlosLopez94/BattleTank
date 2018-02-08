7b8a5f4f13a23764fb92ef627710d48b141ecdda
Carlos <carloslopezrodriguez94@gmail.com>
2018-02-08 17:38:03 +0100

    Setup projectile particle system

--- Source/BattleTank/Projectile.cpp
+++ Source/BattleTank/Projectile.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
@@ -8,6 +10,14 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	collisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh Component"));
+	SetRootComponent(collisionMesh);
+	collisionMesh->SetNotifyRigidBodyCollision(true);
+	collisionMesh->SetVisibility(false);
+
+	launchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Particle System Component"));
+	launchBlast->AttachTo(RootComponent);
+
 	projectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement Component"));
 	projectileMovementComponent->bAutoActivate = false;
 }
